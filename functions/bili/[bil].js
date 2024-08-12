8277ef2fb68233d4744fd64eb20cd2118968f4df
moloo <55693239+moloo2050@users.noreply.github.com>
2024-08-13 00:00:59 +0800

    Update and rename [bili].js to [bil].js

--- functions/bili/[bil].js
+++ functions/bili/[bil].js
@@ -1,5 +1,5 @@
 export async function onRequestGet(context) {
-  const res = await fetch('https://api.live.bilibili.com/room/v1/Room/get_info?from=room&room_id='+context.params.bili);
+  const res = await fetch('https://api.live.bilibili.com/room/v1/Room/get_info?from=room&room_id='+context.params.bil);
   console.log(context.params.bili)
   const live = await res.json();
   console.log(live)
