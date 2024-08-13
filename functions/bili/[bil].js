8d20de89e1dc84181d0fd7ba1cbcd170a720ea0d
moloo <55693239+moloo2050@users.noreply.github.com>
2024-08-13 14:24:31 +0800

    Update [bil].js

--- functions/bili/[bil].js
+++ functions/bili/[bil].js
@@ -1,6 +1,3 @@
 export async function onRequest(context) {
-   const res = await fetch(`https://api.live.bilibili.com/room/v1/Room/get_info?from=room&room_id=${context.params.bil}`);
-  const data = await res.json();
-  const info = JSON.stringify(data);
-  return new Response(info);
+  return await fetch(`https://api.live.bilibili.com/room/v1/Room/get_info?from=room&room_id=${context.params.bil}`);
 }
