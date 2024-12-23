f572470799dc860189cdf038f26fb2a7cfd12724
moloo <55693239+moloo2050@users.noreply.github.com>
2024-12-24 00:19:16 +0800

    Update index.js
    d1

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
-export function onRequest(context) {
-      return new Response("Hello, world!")
-    }
+export async function onRequest(context) {
+  // Create a prepared statement with our query
+  const ps = context.env.qyaoe.prepare("SELECT * from qyplayers");
+  const data = await ps.first();
+
+  return Response.json(data);
+}
