03d049a977ef4b58a80f498e949b9dcdf875da34
moloo <moloisme@163.com>
2024-12-24 21:35:45 +0800

    ii

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  // Create a prepared statement with our query
-  const ps = context.env.qyaoe.prepare("SELECT * from qyplayers");
-  const data = await ps.all();
-
-  return Response.json(data);
-}
