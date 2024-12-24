bb4a007003a70de85d6a2ecac54245e32d941135
moloo <moloisme@163.com>
2024-12-24 11:59:04 +0800

    ff

--- functions/bili/[bili].js
+++
@@ -1,3 +0,0 @@
-export async function onRequest(context) {
-  return await fetch(`https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=`+context.params.bili+`&protocol=0,1&format=0,1,2&codec=0,1&qn=10000`);
-}
