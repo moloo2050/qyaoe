9d7c634767c086f526b5b75396028bfc4c4d7465
moloo <55693239+moloo2050@users.noreply.github.com>
2025-03-11 14:24:33 +0000

    fet

--- src/routes/players/+server.js
+++ src/routes/players/+server.js
@@ -1,5 +1,5 @@
 // @ts-ignore
-export async function onRequest(context) {
+export async function GET(context) {
   // Create a prepared statement with our query
   const ps = context.env.mDB.prepare("SELECT * from qyplayers");
   const data = await ps.first();
