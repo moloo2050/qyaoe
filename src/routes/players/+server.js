db7271fc99184be976ecab95ef68396750bd2b46
moloo <55693239+moloo2050@users.noreply.github.com>
2025-03-12 13:23:14 +0000

    io

--- src/routes/players/+server.js
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-export async function GET(context) {
-  // Create a prepared statement with our query
-  const ps = context.env.mDB.prepare("SELECT * from qyplayers");
-  const data = await ps.first();
-
-  return Response.json(data);
-}
