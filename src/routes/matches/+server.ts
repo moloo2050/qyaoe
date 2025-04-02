555389609b19c895fdd54da51b3ca75a98b7277f
moloo <55693239+moloo2050@users.noreply.github.com>
2025-04-02 11:05:50 +0000

    d1

--- src/routes/matches/+server.ts
+++ src/routes/matches/+server.ts
@@ -3,6 +3,6 @@ import type { RequestHandler } from "@sveltejs/kit";
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ request, platform }) {
   let result = await platform.env.mDB.prepare(
-    "SELECT * FROM match",
+    "SELECT * FROM match LIMIT 500",
   ).run();
   return new Response(JSON.stringify(result.results));}
