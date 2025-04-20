3a90e4552f9b2e074956996ada7193e6e84b1c58
moloo <55693239+moloo2050@users.noreply.github.com>
2025-04-20 10:50:55 +0000

    3000

--- src/routes/matches2000/+server.ts
+++ src/routes/matches2000/+server.ts
@@ -3,6 +3,6 @@ import type { RequestHandler } from "@sveltejs/kit";
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function GET({ request, platform }) {
   let result = await platform.env.mDB.prepare(
-    "SELECT * FROM match ORDER BY id LIMIT 1000",
+    "SELECT * FROM match ORDER BY id 1000  OFFSET 2000",
   ).run();
   return new Response(JSON.stringify(result.results));}
