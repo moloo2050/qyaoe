69c455cc6b7df5d5a83061f15c583aae2749ea35
moloo <55693239+moloo2050@users.noreply.github.com>
2025-08-06 12:28:54 +0000

    uuu

--- src/routes/oldes/+server.js
+++ src/routes/oldes/+server.js
@@ -8,7 +8,7 @@ export async function GET({fetch}) {
   }}
   const matches=[]
   // @ts-ignore
-  const match =  await fetch('https://aoe2.pages.dev/matches', options).then(res => res.json())
+  const match =  await fetch('https://aoe2.pages.dev/newmatches', options).then(res => res.json())
   for (let i = 0; i < match.length; i++) {
     const element = JSON.parse(match[i].matchelo);
     matches.push(element)
